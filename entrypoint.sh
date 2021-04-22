9660b55d4142274449cb33059db3d1600c4f8ab6
Marcello Cordeiro <msc3@cin.ufpe.br>
2021-04-22 02:11:55 -0300

    Update Shaker repository

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-git clone --depth 1 https://github.com/marcellocordeiro/stress-script.git ./__stress_script
-python3 "./__stress_script/shaker.py" $INPUT_TESTING_TOOL "." -o "./__stress_output" -nsr $INPUT_NO_STRESS_RUNS -sr $INPUT_STRESS_RUNS
+git clone --depth 1 https://github.com/marcellocordeiro/shaker-standalone.git ./__shaker
+python3 "./__shaker/shaker.py" $INPUT_TESTING_TOOL "." -o "./__shaker_output" -nsr $INPUT_NO_STRESS_RUNS -sr $INPUT_STRESS_RUNS
