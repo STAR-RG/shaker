a8b066019369aeadb4e3bede638c5bc1426be777
sorattorafa <rampimsoratto@gmail.com>
2023-03-19 22:48:13 -0300

    feat: add post tests to jest

--- shaker/tool_pytest.py
+++ shaker/tool_pytest.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 from base_tool import BaseTool
 from util import subprocess_run
-
+from shutil import copy
 
 class Pytest(BaseTool):
     def setup(self):
@@ -22,3 +22,14 @@ class Pytest(BaseTool):
         )
         # print(f"> {command}")
         subprocess_run(command, cwd=str(self.directory))
+
+    def post_tests(self, report_folder):
+        # Copy reports
+        report_folder.mkdir(parents=True, exist_ok=True)
+
+        reports = self.directory.rglob("TEST-*.xml")
+
+        for report in reports:
+            dest = report_folder / report.name
+            if report != dest:
+                copy(report, dest)
