6f9c06f1d0c45618c2b2efb034b6cf508894b13b
sorattorafa <rampimsoratto@gmail.com>
2023-03-19 22:59:42 -0300

    fix: remove unecessary file

--- shaker/tool_pytest.py
+++ shaker/tool_pytest.py
@@ -22,14 +22,3 @@ class Pytest(BaseTool):
         )
         # print(f"> {command}")
         subprocess_run(command, cwd=str(self.directory))
-
-    def post_tests(self, report_folder):
-        # Copy reports
-        report_folder.mkdir(parents=True, exist_ok=True)
-
-        reports = self.directory.rglob("TEST-*.xml")
-
-        for report in reports:
-            dest = report_folder / report.name
-            if report != dest:
-                copy(report, dest)
