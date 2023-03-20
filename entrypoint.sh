6af50408d3c207aa792f2fd291aea2ba19a80b9b
sorattorafa <38047989+sorattorafa@users.noreply.github.com>
2023-03-20 00:14:03 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,7 +16,7 @@ do
   done
 done
 
-echo tests
+echo $tests
 # python3 "./__shaker/analytics.py" "./__shaker_output" $GITHUB_REPOSITORY $GITHUB_SHA $tests $INPUT_NO_STRESS_RUNS $INPUT_RUNS
 
 exit $ret
