d1af155e584a439b0c31344a7caac6dc4be44293
sorattorafa <rampimsoratto@gmail.com>
2023-03-30 23:14:13 -0300

    feat: create save simplified flakies

--- shaker/analytics.py
+++ shaker/analytics.py
@@ -7,69 +7,70 @@ from pathlib import Path
 # repoUrl = "http://localhost:3000/repos"
 # flakiesUrl = "http://localhost:3000/flakies"
 
-with open(Path('./output/') / "__results.json") as f:
-    parser = ArgumentParser()
-    parser.add_argument("output_folder", help="output folder")
-    parser.add_argument("repo", help="repository")
-    parser.add_argument("ref", help="repository ref")
-    parser.add_argument("numtests", type=int, help="number of tests")
-    parser.add_argument("nsr", type=int, help="specify number of no-stress runs")
-    parser.add_argument("sr", type=int, help="specify number of stress runs")
-    args = parser.parse_args()
-
-    total_runs = args.nsr + (args.sr * 4)
+def save_simplified_flakies():
     
-    parser = ArgumentParser()
-    parser.add_argument("output_folder", help="output folder")
-    parser.add_argument("repo", help="repository")
-    parser.add_argument("ref", help="repository ref")
-    parser.add_argument("numtests", type=int, help="number of tests")
-    parser.add_argument("nsr", type=int, help="specify number of no-stress runs")
-    parser.add_argument("sr", type=int, help="specify number of stress runs")
-    args = parser.parse_args()
+    with open(Path('./output/') / "__results.json") as f:
+        parser = ArgumentParser()
+        parser.add_argument("output_folder", help="output folder")
+        parser.add_argument("repo", help="repository")
+        parser.add_argument("ref", help="repository ref")
+        parser.add_argument("numtests", type=int, help="number of tests")
+        parser.add_argument("nsr", type=int, help="specify number of no-stress runs")
+        parser.add_argument("sr", type=int, help="specify number of stress runs")
+        args = parser.parse_args()
 
-    total_runs = args.nsr + (args.sr * 4)
+        total_runs = args.nsr + (args.sr * 4)
+        
+        parser = ArgumentParser()
+        parser.add_argument("output_folder", help="output folder")
+        parser.add_argument("repo", help="repository")
+        parser.add_argument("ref", help="repository ref")
+        parser.add_argument("numtests", type=int, help="number of tests")
+        parser.add_argument("nsr", type=int, help="specify number of no-stress runs")
+        parser.add_argument("sr", type=int, help="specify number of stress runs")
+        args = parser.parse_args()
+
+        total_runs = args.nsr + (args.sr * 4)
 
 
-    repoInfo = {"name": args.repo, "ref": args.ref, "num_tests": args.numtests}
-    repoJson = json.loads(json.dumps(repoInfo))
-    failures = json.load(f)
-    
-    for module in failures:
-        moduleName = module
+        repoInfo = {"name": args.repo, "ref": args.ref, "num_tests": args.numtests}
+        repoJson = json.loads(json.dumps(repoInfo))
+        failures = json.load(f)
+        
+        for module in failures:
+            for test_case in failures[module]:
+                testCaseName = test_case
 
-        for test_case in failures[module]:
-            testCaseName = test_case
+                function_failures = failures[module][test_case]
 
-            function_failures = failures[module][test_case]
-
-            no_stress_failures = 0
-            stress_failures = 0
-
-            for failure in function_failures:
-                if failure["config"] == "no-stress":
-                    no_stress_failures += 1
-                else:
-                    stress_failures += 1
-
-            total_failures = no_stress_failures + stress_failures
-            ratio = total_failures / total_runs
-
-            flaky = {
-                "repo": repoInfo["name"],
-                "module": module,
-                "function_name": testCaseName,
-                "datetime_epoch": int(time.time()),
-                "ratio": ratio,
-            }
-            flakyJson = json.loads(json.dumps(flaky))
-
-            flakyPostObject = {
-                "repoInfos": repoJson,
-                "flakyJson": flakyJson,
-            }
-
-            post("https://flakybd-97b53-default-rtdb.firebaseio.com/.json", json=flakyPostObject)
+                no_stress_failures = 0
+                stress_failures = 0
+                tests_run_configurations_type = 'plain'
+                for failure in function_failures:
+                    if failure["config"] == "no-stress":
+                        no_stress_failures += 1
+                    else:
+                        tests_run_configurations_type = 'stress'
+                        stress_failures += 1
+                    flakyPostObject = {
+                        "project_name": repoInfo["name"],
+                        "project_commit": repoInfo["ref"],
+                        "tests_run_configurations_type": tests_run_configurations_type,
+                        "run_times": total_runs,
+                        "module_name": module,
+                        "test_case_name": testCaseName,
+                        "created_at": int(time.time()),
+                        "test_case_result": failure,
+                    }
+                    post("https://flakybd-97b53-default-rtdb.firebaseio.com/.json", json=flakyPostObject)
+                
+                #total_failures = no_stress_failures + stress_failures
+                #ratio = total_failures / total_runs
+                #flakyJson = json.loads(json.dumps(flaky))
+                #flakyPostObject = {
+                ##    "repoInfos": repoJson,
+                ##    "flakyJson": flakyJson,
+                #}
 
 def default_post():
     repoUrl = "https://my-new-app-denini.herokuapp.com/repos"
@@ -125,4 +126,5 @@ def default_post():
             flakyJson = json.loads(json.dumps(flaky))
             post(flakiesUrl, json=flakyJson)
 
+save_simplified_flakies()
 # / == %2F
